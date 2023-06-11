1c94615c2d2e1fede9136edf62f0758e05a890db
Couto1411 <gabriel.couto14@hotmail.com>
2023-06-11 12:30:36 -0300

    exemplo

--- todolistdb/src/main/Routes.jsx
+++ todolistdb/src/main/Routes.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Login from '../components/Login/login';
 import { useNavigate } from "react-router-dom"
 
 export default function Props(){
     const navigate = useNavigate();
     return <Routes>
-        <Route path='/' element={<div navigate={navigate}></div>} exact/>
+        <Route path='/login' element={<Login navigate={navigate}/>} exact/>
     </Routes>
 }
