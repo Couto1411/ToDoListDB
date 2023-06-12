68a3508c5173729a615a3275b57eb4c713e582ce
cefet-5 <95004455+cefet-5@users.noreply.github.com>
2023-06-12 13:29:23 -0300

    pablo1

--- frontend/src/components/Tarefas/tarefas.jsx
+++ frontend/src/components/Tarefas/tarefas.jsx
@@ -2,14 +2,62 @@ import React from "react";
 import "./tarefas.css"
 import MenuPage from "../Menu/menupage";
 
-export default function Tarefas(){
-    const secao = () =>{
-        return <section>
-                TAREFAS
-            </section>
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
+export default function Tarefas() {
+
+    const [usuTar, setUsuTar] = React.useState(false);
+
+    const Tarefass = {
+        "nome": "Lista 1",
+        "tarefa": [
+            {
+                "titulo": "titulo 1",
+                "descricao": "werwe"
+            },
+                {
+                    "titulo": "titulo 2",
+                    "descricao": "wfsdfsd"
+                },
+        ]
     }
 
-    return(
-        <MenuPage secao={secao}/>
+    function renderTarefas() {
+        return <div>
+            <div>Tarefas</div>
+            {Tarefass.tarefa.map(tarefa=>{
+                return <div>{tarefa.titulo}<br/></div>
+            })}
+        </div>
+    }
+
+    function renderUsu() {
+        return <div style={{display: "inline-block"}}>
+            <span style={{marginRight: 120}}>Tarefas</span>
+            <span style={{marginLeft: 120}}>Usuários</span>
+        </div>
+    }
+
+    const secao = () => {
+        return <section>
+            <Typography align="center" variant="h4">
+                <div>
+
+                    <Button onClick={() => { setUsuTar(!usuTar) }}> Botão</Button>
+                    {usuTar ?
+                        renderTarefas() :
+                        renderUsu()}
+                </div>
+            </Typography>
+
+            {Tarefas.tarefa}
+
+        </section>
+    }
+
+    return (
+        <MenuPage secao={secao} />
     )
 }
