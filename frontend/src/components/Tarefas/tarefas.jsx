fd1f728ddb45442d8570af8c73e2ff9ce4f506b6
Pablo <pablossilva.005@gmail.com>
2023-06-12 21:57:54 -0300

    halfway there

--- frontend/src/components/Tarefas/tarefas.jsx
+++ frontend/src/components/Tarefas/tarefas.jsx
@@ -7,39 +7,40 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
+//import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
-import Avatar from '@mui/material/Avatar';
+//import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Grid from '@mui/material/Grid';
-import FolderIcon from '@mui/icons-material/Folder';
+//import FolderIcon from '@mui/icons-material/Folder';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Pagination from '@mui/material/Pagination';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 
 
 export default function Tarefas() {
 
     const [dense] = React.useState(false);
 
-    const [Tar, setTar] = React.useState(false);
+    const [Tar, setTar] = React.useState(true);
 
     const Tarefass = {
         "nome": "Lista 1",
         "tarefa": [
             {
-                "titulo": "Título 1",
-                "descricao": "werwe"
+                "titulo": "Tarefa 1"
             },
             {
-                "titulo": "Título 2",
-                "descricao": "wfsdfsd"
+                "titulo": "Tarefa 2"
             },
         ],
         "usuarios": [
             {
-                "usuario": "Usuário 1"
+                "usuario": "Usuário Compartilhado 1"
             },
             {
-                "usuario": "Usuário 2"
+                "usuario": "Usuário Compartilhado 2 "
             },
         ]
     }
@@ -74,14 +75,27 @@ export default function Tarefas() {
         })
     }
 
+    function titulo() {
+        return <Box variant="3" align>
+            <Box edge="end" align="center" className="TEKOtarefa">
+                {Tarefass.nome}
+            </Box>
+            <IconButton edge="end" aria-label="delete">
+                <DeleteIcon sx={{ color: "red" }} />
+            </IconButton>
+        </Box>
+
+    }
+
     const secao = () => {
         return <section>
+
+            {titulo()}
 
             <Box align="center" display="flex">
                 <Button onClick={() => { setTar(true) }} sx={{ flexGrow: 1 }}>
                     <Typography display="inline">Tarefas</Typography>
                 </Button>
-
 
                 <Button onClick={() => { setTar(false) }} sx={{ flexGrow: 1 }}>
                     <Typography display="inline">Usuários</Typography>
@@ -89,22 +103,16 @@ export default function Tarefas() {
 
             </Box>
             {Tar &&
-                <Grid item xs={12} md={6}>
-                    {/* <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-                        Avatar with text and icon
-                    </Typography> */}
-
+                <Grid>
                     <List dense={dense}>
                         {renderTarefas()}
                     </List>
                 </Grid>
             }
 
-            {!Tar &&
-                renderUsu()
-            }
+            {!Tar && renderUsu()}
 
-            {/* {Tarefas.tarefa} */}
+            <Pagination count={10} className="pagination" />
 
         </section>
     }
